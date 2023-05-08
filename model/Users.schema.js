d82e3047ef5cdc813789da1ebc5580c6916498b7
Iskyy <iskycruise01@gmail.com>
2023-05-08 07:32:06 +0700

    Add timestamp on schema

--- model/Users.schema.js
+++ model/Users.schema.js
@@ -1,38 +1,43 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    trim: true,
-  },
-
-  raw_password: {
-    type: String,
-  },
-
-  hash_password: {
-    type: String,
-  },
-
-  posts: [
-    {
-      title: {
-        type: String,
-        trim: true,
-      },
-
-      content: {
-        type: String,
-        trim: true,
-      },
-
-      date: {
-        type: Date,
-        default: new Date(),
-      },
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      trim: true,
     },
-  ],
-});
+
+    raw_password: {
+      type: String,
+    },
+
+    hash_password: {
+      type: String,
+    },
+
+    posts: [
+      {
+        title: {
+          type: String,
+          trim: true,
+        },
+
+        content: {
+          type: String,
+          trim: true,
+        },
+
+        date: {
+          type: Date,
+          default: new Date(),
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const User = mongoose.model("Users", UserSchema);
 module.exports = User;
