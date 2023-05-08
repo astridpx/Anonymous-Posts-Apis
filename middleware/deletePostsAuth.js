287b4d432d500aa6580d450fe2d5320862c10e90
Iskyy <iskycruise01@gmail.com>
2023-05-08 17:03:10 +0700

    Created a deleteposts middleware to protect the routes

---
+++ middleware/deletePostsAuth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  const token = req.header("bearer");
+
+  // ? Check if Token is available
+  if (!token) return res.status(404).send({ message: "No token found." });
+
+  try {
+    const user = await jwt.verify(token, process.env.TOKEN_KEY);
+
+    if (user.userid !== req.params.id)
+      return res.status(408).send({ message: "You cant delete this posts" });
+
+    next();
+  } catch (error) {
+    res.status(400).send({ message: "Invalid Token." });
+  }
+};
